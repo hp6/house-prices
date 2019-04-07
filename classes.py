4eaa6842213ba3afb25bc5bcc67a61766b209356
unknown <hayk8128@gmail.com>
2019-04-07 17:44:22 +0400

    add data groups by type and minor

--- classes.py
+++ classes.py
@@ -2,3 +2,17 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
 
+class MasVnrImputer(BaseEstimator, TransformerMixin):
+    def __init__(self, type_fill_val="None", area_fill_val=0):
+        self.type_fill_val = type_fill_val
+        self.area_fill_val = area_fill_val
+    
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        X.loc[X["MasVnrType"].isnull(), "MasVnrType"] = self.type_fill_val
+        X.loc[X["MasVnrArea"].isnull(), "MasVnrArea"] = self.area_fill_val
+        return X
+        
+    
--- functions.py
+++ functions.py
@@ -1,1 +1,5 @@
 import pandas as pd
+import numpy as np
+
+def lists_eq(list1, list2):
+    return np.sum(np.sort(list1) == np.sort(list2)) == len(list1)
