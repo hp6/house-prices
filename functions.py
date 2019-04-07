e30ed34baf7ae89f697a9f7dfd02072e66df426e
unknown <hayk8128@gmail.com>
2019-04-07 23:56:11 +0400

    minor

--- functions.py
+++ functions.py
@@ -4,3 +4,8 @@ import numpy as np
 def lists_eq(list1, list2):
     return np.sum(np.sort(list1) == np.sort(list2)) == len(list1)
 
+def unique_values(df):
+    unique_values = []
+    for column in df:
+        unique_values.append(df[column].unique())
+    return unique_values
